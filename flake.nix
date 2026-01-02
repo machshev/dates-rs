56c211dc6db09f7c1b7ba478b30602cc7e22a945
James McCorrie <djmccorrie@gmail.com>
2026-01-02 22:36:37 +0000

    Initial commit
    Signed-off-by: David James McCorrie <djmccorrie@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay/stable";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs:
+    with inputs;
+      flake-utils.lib.eachDefaultSystem (
+        system: let
+          overlays = [(import rust-overlay)];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+        in {
+          devShells = {
+            default = with pkgs;
+              mkShell {
+                buildInputs = [
+                  (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))
+                  cargo-nextest
+                  cargo-udeps
+                  cargo-vet
+                  cargo-about
+                  cargo-release
+                  cargo-machete
+
+                  rust-analyzer
+                  rustfmt
+
+                  adrs
+                  typos
+
+                  # If the dependencies need system libs, you usually need pkg-config + the lib
+                  pkg-config
+                  openssl
+                ];
+              };
+          };
+
+          formatter = nixpkgs.legacyPackages.${system}.alejandra;
+        }
+      );
+}
